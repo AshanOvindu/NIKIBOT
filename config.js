ad0b800386a2689413ec96f759f98ab230087866
AshanOvindu <ashanovindu5@gmail.com>
2024-08-24 15:08:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "ENTER MONGODB URL",
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~KJ8yWS4R#5aR0m-aJ9xv_-niq79DdEawN82cnF9jgshedgzP6IiM"
+MONGODB: process.env.MONGODB || "mongodb+srv://vihangadesilva0527:niki0220@cluster0.gkwuvqk.mongodb.net/",
 };
